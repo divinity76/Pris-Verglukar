3c3aa9517d9c46477ed3c1c6cafc4a82267798b6
divinity76 <divinity76@gmail.com>
2020-11-08 18:04:10 +0100

    proper html encoding

--- IkeaPriceComparison.js
+++ IkeaPriceComparison.js
@@ -8,10 +8,7 @@
 window.addEventListener("load", function () {
   function addOtherPrice() {
     const uri = window.location.href;
-
     const language = "nl/nl"; // Input the country code of the website you wish to compare to in this format '../..'
-    const currency = "€";
-
     const productId = document.getElementsByClassName(
       "range-revamp-product-identifier__number"
     )[0].innerText;
@@ -28,13 +25,13 @@ window.addEventListener("load", function () {
       })
       .then(function (result) {
         try {
-          otherPrice = ` ${currency}${result.searchResultPage.productWindow[0].priceNumeral}`;
+          otherPrice =
+            " €" + result.searchResultPage.productWindow[0].priceNumeral;
           otherSiteUri = result.searchResultPage.productWindow[0].pipUrl;
         } catch {
           available = false;
           otherPrice = " N/A in other country";
         }
-
         let otherPriceSpan = document.createElement("span");
         let otherPricetextnode = document.createTextNode(otherPrice);
         otherPriceSpan.appendChild(otherPricetextnode);
@@ -47,12 +44,14 @@ window.addEventListener("load", function () {
           )[0];
 
           let otherSiteButton = document.createElement("button");
-          otherSiteButton.innerHTML = `<a href="${otherSiteUri}">Take me there</a>`;
-          otherSiteButton.firstChild.style.textDecorationStyle;
+          let a = document.createElement("a");
+          a.textContent = "Take me there";
+          a.href = otherSiteUri;
+          otherSiteButton.appendChild(a);
+          a.style.textDecorationStyle;
           divToAppend.appendChild(otherSiteButton);
         }
       });
   }
   addOtherPrice();
 });
-
